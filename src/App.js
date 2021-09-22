aca01ea574f247e7d00e39a96b9d8f3b99c78d56
Johan Rodriguez <johanpaolorodriguez@gmail.com>
2021-09-22 16:45:55 +0800

    feat: add browser-routing & remove intializion bloat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Footer from "./components/navigation/Footer";
+import Navbar from "./components/navigation/Navbar";
+import DirectoryPage from "./pages/DirectoryPage";
+import HomePage from "./pages/HomePage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <p>Initial State</p>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/directory" component={ DirectoryPage } />
+        <Route exact path="/" component={ HomePage } />
+      </Switch>
+      <Footer />
     </div>
-  );
+  );            
 }
 
 export default App;
