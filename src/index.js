9626a8950768d9c409c3b43650e28ac7480dcea0
Johan Rodriguez <johanpaolorodriguez@gmail.com>
2021-09-25 12:12:29 +0800

    feat(files): add basic components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import App from './components/App'
 
 const pageComponent = (
   <BrowserRouter>
@@ -16,4 +15,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals();
