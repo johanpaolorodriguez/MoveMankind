218f4dda6355ea54c60128a5ad62af1e6125a392
Johan Rodriguez <johanpaolorodriguez@gmail.com>
2021-09-25 15:00:14 +0800

    build(firebase): add firebase package, initialize it, and connect it to our application

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import App from './components/App'
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 const pageComponent = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <FirebaseContext.Provider value={ new Firebase() }>
+      <App />
+    </FirebaseContext.Provider>
 )
 
 ReactDOM.render(
