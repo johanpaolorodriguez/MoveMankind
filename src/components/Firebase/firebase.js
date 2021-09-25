937fb1e2b6da571994c9342665338b6c01bafbbd
Johan Rodriguez <johanpaolorodriguez@gmail.com>
2021-09-25 20:23:18 +0800

    feat(form): add ability for user to sign-up with email and password

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -12,7 +13,15 @@ const config = {
 class Firebase {
     constructor() {
         app.initializeApp(config);
+        this.auth = app.auth();
     }
+
+    // *** AUTH API ***
+    doCreateUserWithEmailAndPassword = (email, password) => this.auth.createUserWithEmailAndPassword(email, password);
+    doSignInWithEmailAndPassword = (email, password) => this.auth.signInWithEmailAndPassword(email, password);
+    doSignOut = () => this.auth.signOut();
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+    doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -8,6 +8,10 @@ const Navigation = () => (
             <li>
                 <Link to={ROUTES.SIGN_IN}>Sign In</Link>
             </li>
+
+            <li>
+                <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+            </li>
             
             <li>
                 <Link to={ROUTES.LANDING}>Landing</Link>
