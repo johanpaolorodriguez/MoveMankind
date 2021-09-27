bf181ab37e2e6e935f021cb6ecdfd168ee2e1167
Johan Rodriguez <johanpaolorodriguez@gmail.com>
2021-09-27 09:15:47 +0800

    refactor(sign-up): change sign-up form to make use of hooks and custom input-hook

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link, withRouter } from 'react-router-dom'
 import { compose } from "recompose";
 import { withFirebase } from "../Firebase";
 import * as ROUTES from '../../constants/routes'
+import { useInput } from "../Hooks/input-hook";
 
 const SignUpPage = () => (
     <div>
@@ -11,94 +12,65 @@ const SignUpPage = () => (
     </div>
 );
 
-const INITIAL_STATE ={
-    username: '',
-    email: '',
-    passwordOne: '',
-    passwordTwo: '',
-    error: null
-}
-
-class SignUpFormBase extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { ...INITIAL_STATE };
-
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onChange = this.onChange.bind(this);
-    }
-    onSubmit = event => {
-        const { username, email, passwordOne } = this.state;
-
-        this.props.firebase
-        .doCreateUserWithEmailAndPassword(email, passwordOne)
+const SignUpFormBase = (props) => {
+    const { value:username, bind:bindUsername, reset:resetUsername } = useInput("");
+    const { value:email, bind:bindEmail, reset:resetEmail } = useInput("");
+    const { value:passwordOne, bind:bindPasswordOne, reset:resetPasswordOne } = useInput("");
+    const { value:passwordTwo, bind:bindPasswordTwo, reset:resetPasswordTwo } = useInput("");
+    const [error, setError] = useState("");
+    
+    const onSubmit = event => {
+        props.firebase.doCreateUserWithEmailAndPassword(email, passwordOne)
         .then(authUser => {
-            this.setState({ ...INITIAL_STATE });
-            this.props.history.push(ROUTES.HOME)
+            resetUsername();
+            resetEmail();
+            resetPasswordOne();
+            resetPasswordTwo();
+            props.history.push(ROUTES.HOME)
         })
         .catch(error => {
-            this.setState({error});
-        });
-        event.preventDefault();
-    }
-    
-    onChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
+            setError(error);
+        })
+        event.preventDefault()
     }
 
-    render() {
-        const {
-            username,
-            email,
-            passwordOne,
-            passwordTwo,
-            error
-        } = this.state;
-
-        const isInvalid =
+    const isInvalid =
             passwordOne !== passwordTwo ||
             passwordOne === '' ||
             email === '' ||
             username === '';
 
-        return(
-            <form onSubmit={this.onSubmit}>
-                <input
-                    name="username"
-                    value={ username }
-                    onChange={ this.onChange }
-                    type="text"
-                    placeholder="Full Name"
-                />
-                <input
-                    name="email"
-                    value={ email }
-                    onChange={ this.onChange }
-                    type="email"
-                    placeholder="Email"
-                />
-                <input
-                    name="passwordOne"
-                    value={ passwordOne }
-                    onChange={ this.onChange }
-                    type="password"
-                    placeholder="Password"
-                />
-                <input
-                    name="passwordTwo"
-                    value={ passwordTwo }
-                    onChange={ this.onChange }
-                    type="password"
-                    placeholder="Confirm Password"
-                />
-                <button disabled={ isInvalid } type="submit">Sign Up</button>
+    return (
+        <form onSubmit={ onSubmit }>
+        <input
+            { ...bindUsername }
+            name="username"
+            type="text"
+            placeholder="Full Name"
+        />
+        <input
+            { ...bindEmail }
+            name="email"
+            type="email"
+            placeholder="Email"
+        />
+        <input
+            { ...bindPasswordOne }
+            name="passwordOne"
+            type="password"
+            placeholder="Password"
+        />
+        <input
+            { ...bindPasswordTwo }
+            name="passwordTwo"
+            type="password"
+            placeholder="Confirm Password"
+        />
+        <button disabled={ isInvalid } type="submit">Sign Up</button>
 
-                { error && <p>{ error.message }</p> }
-            </form>
-        )
-    }
-
+        { error && <p>{ error.message }</p> }
+    </form>
+    )
 }
 
 const SignUpLink = () => (
