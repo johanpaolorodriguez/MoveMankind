1d7c892ebfc3653104f4612a22bfd05b55f4f493
Johan Rodriguez <johanpaolorodriguez@gmail.com>
2021-10-16 18:40:53 +0800

    style(tailwind): add classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,20 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'primary': ["Montserrat"],
-        'secondary': ["Open Sans"],
+        primary: ["Montserrat"],
+        secondary: ["Open Sans"],
       },
       colors: {
-        'primary': "#232535",
-        'secondary': "#585C7D",
-        'accent1': "#FF9AA2",
-        'accent2': "#FFDAC1",
-        'accent3': "#E2F0CB",
-        'accent4': "#C7CEEA",
+        primary: "#232535",
+        secondary: "#585C7D",
+        accent1: "#FF9AA2",
+        accent2: "#FFDAC1",
+        accent3: "#E2F0CB",
+        accent4: "#C7CEEA",
+      },
+      height: {
+        "screen-1/3": "33vh",
+        "screen-1/2": "50vh",
       },
     },
   },
