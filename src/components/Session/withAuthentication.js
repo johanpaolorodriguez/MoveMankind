0a12fd090bcbe0c9e237f40955d6eeb059c8cb2f
Johan Rodriguez <johanpaolorodriguez@gmail.com>
2021-11-06 10:17:09 +0800

    fix(session): add missing useEffect dependency to fix infinite authentication listener render loop

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -2,30 +2,28 @@ import React, { useState, useEffect } from "react";
 import AuthUserContext from "./context";
 import { withFirebase } from "../Firebase";
 
-const withAuthentication = Component => {
-    const WithAuthentication = (props) => {
-        const [authUser, setAuthUser] = useState(null);
-        
-        useEffect(() => {
-            //get the authenticated user from Firebase
-            let listener = props.firebase.auth.onAuthStateChanged(authUser => {
-                authUser
-                ? setAuthUser({ authUser })
-                : setAuthUser( null );
-            })
+const withAuthentication = (Component) => {
+  const WithAuthentication = (props) => {
+    const [authUser, setAuthUser] = useState(null);
 
-            return () => {
-                listener();
-            }
-        }) 
+    useEffect(() => {
+      //get the authenticated user from Firebase
+      let listener = props.firebase.auth.onAuthStateChanged((authUser) => {
+        authUser ? setAuthUser({ authUser }) : setAuthUser(null);
+      });
 
-        return(
-            <AuthUserContext.Provider value={ authUser }>
-                <Component { ...props } />
-            </AuthUserContext.Provider>
-        );                      
-    }
-    return withFirebase(WithAuthentication);
-}
+      return () => {
+        listener();
+      };
+    }, [props.firebase.auth]);
+
+    return (
+      <AuthUserContext.Provider value={authUser}>
+        <Component {...props} />
+      </AuthUserContext.Provider>
+    );
+  };
+  return withFirebase(WithAuthentication);
+};
 
 export default withAuthentication;
