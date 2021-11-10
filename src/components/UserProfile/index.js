070e3cf4dad5b3341fae2e3bd10a3cf0f3b23f41
Johan Rodriguez <johanpaolorodriguez@gmail.com>
2021-11-10 10:50:37 +0800

    fix(UserProfile): correct the import module for useState

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,9 +1,6 @@
-import React, { useEffect } from "react";
-import { useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { useState } from "react/cjs/react.development";
 import { withFirebase } from "../Firebase";
-import { AuthUserContext } from "../Session";
 import Table from "../StartUps/table";
 
 /**
@@ -14,8 +11,7 @@ import Table from "../StartUps/table";
 
 const UserProfilePage = (props) => {
   const { uid } = useParams();
-  const authUser = useContext(AuthUserContext);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState();
   const [startups, setStartups] = useState([]);
 
   //mount user profile
@@ -23,14 +19,12 @@ const UserProfilePage = (props) => {
   useEffect(() => {
     const getUserData = async () => {
       try {
-        const user = await props.firebase.getUserByID(uid);
-        console.log(user.following);
-        setUser(user);
+        const userData = await props.firebase.getUserByID(uid);
+        setUser(userData);
         const startups = await props.firebase.getAllStartupsWithID(
-          user.following
+          userData.following
         );
         setStartups(startups);
-        console.log(user);
       } catch (error) {
         console.log(error);
       }
@@ -40,34 +34,39 @@ const UserProfilePage = (props) => {
 
   return (
     <div className="max-w-6xl mx-auto">
-      <div className="space-y-8">
-        <h1 className="mt-16 text-3xl font-bold text-primary">Profile</h1>
-        <article className="flex space-x-6">
-          <img
-            className="inline-block w-24 h-24 rounded-full ring-2 ring-white"
-            src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            alt=""
-          />
-          <div className="">
-            <h2 className="text-4xl font-bold">{user.username}</h2>
-            <p className="text-xl">{user.email}</p>
-            <p className="text-xl">Boston, MA, USA</p>
-          </div>
-        </article>
-        <p className="">
-          Buck drives strategic partnerships and corporate development at Miso.
-          As a technologist and early-stage venture investor, he has a track
-          record of building successful businesses at the leading edge of
-          technology in transformative, high growth markets. He has held roles
-          at Wavemaker Partners and Wavemaker Labs, and founded VC firm Canyon
-          Creek Capital.
-        </p>
-      </div>
-
-      {startups.length !== 0 ? (
+      {user ? (
         <div>
-          <h1 className="mt-16 text-3xl font-bold text-primary">Ventures</h1>
-          <Table startups={startups} />
+          <div className="space-y-8">
+            <h1 className="mt-16 text-3xl font-bold text-primary">Profile</h1>
+            <article className="flex space-x-6">
+              <img
+                className="inline-block w-24 h-24 rounded-full ring-2 ring-white"
+                src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div className="">
+                <h2 className="text-4xl font-bold">{user.username}</h2>
+                <p className="text-xl">{user.email}</p>
+                <p className="text-xl">Boston, MA, USA</p>
+              </div>
+            </article>
+            <p className="">
+              Buck drives strategic partnerships and corporate development at
+              Miso. As a technologist and early-stage venture investor, he has a
+              track record of building successful businesses at the leading edge
+              of technology in transformative, high growth markets. He has held
+              roles at Wavemaker Partners and Wavemaker Labs, and founded VC
+              firm Canyon Creek Capital.
+            </p>
+          </div>
+          {startups.length !== 0 ? (
+            <div>
+              <h1 className="mt-16 text-3xl font-bold text-primary">
+                Ventures
+              </h1>
+              <Table startups={startups} />
+            </div>
+          ) : null}
         </div>
       ) : null}
     </div>
