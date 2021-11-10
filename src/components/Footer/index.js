e831c60054c3ff509a695bf7571e48af3bef027f
Johan Rodriguez <johanpaolorodriguez@gmail.com>
2021-11-10 10:51:30 +0800

    style(footer): change footer position to be always at the bottom of the page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex items-center w-full h-12 bg-primary">
+    <div className="absolute bottom-0 left-0 right-0 flex items-center w-full h-12 bg-primary">
       <div className="w-full mx-auto max-w-7xl">
         <p className="w-full font-medium text-white">
           © 2021 Move Mankind. All rights reserved
