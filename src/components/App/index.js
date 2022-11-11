bcbcd9e7325ca5ad01e17a26bd756efab7506648
johanpaolorodriguez <johanpaolorodriguez@gmail.com>
2022-11-11 20:05:43 +0800

    refactor(routes): add directory to routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,24 +12,29 @@ import AdminPage from "../Admin";
 import StartUpsPage from "../StartUps";
 import UserProfilePage from "../UserProfile";
 import Footer from "../Footer";
+import ViewStartUpPage from "../ViewStartUp";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  return (
-    <Router>
-      <Navigation />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route exact path={ROUTES.STARTUPS} component={StartUpsPage} />
-      <Route path={ROUTES.CATEGORY} component={StartUpsPage} />
-      <Route path={ROUTES.PROFILE} component={UserProfilePage} />
-      <Toaster position="top-right" reverseOrder={false} />
-    </Router>
-  );
+	return (
+		<Router>
+			<Navigation />
+			<Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+			<Route path={ROUTES.SIGN_IN} component={SignInPage} />
+			<Route
+				path={ROUTES.PASSWORD_FORGET}
+				component={PasswordForgetPage}
+			/>
+			<Route path={ROUTES.ACCOUNT} component={AccountPage} />
+			<Route path={ROUTES.ADMIN} component={AdminPage} />
+			<Route exact path={ROUTES.LANDING} component={LandingPage} />
+			<Route exact path={ROUTES.STARTUPS} component={StartUpsPage} />
+			<Route path={ROUTES.CATEGORY} component={StartUpsPage} />
+			<Route path={ROUTES.PROFILE} component={UserProfilePage} />
+			<Route path="/startups/:uid" component={ViewStartUpPage} />
+			<Toaster position="top-right" reverseOrder={false} />
+		</Router>
+	);
 };
 
 export default withAuthentication(App);
