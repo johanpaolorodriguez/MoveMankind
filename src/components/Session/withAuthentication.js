7e71242746a5541d250980435fd5a5e811881f0e
johanpaolorodriguez <johanpaolorodriguez@gmail.com>
2022-11-28 17:33:21 +0800

    feat(admin): add admin claim to authenticated user context

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -3,36 +3,46 @@ import AuthUserContext from "./context";
 import { withFirebase } from "../Firebase";
 
 const withAuthentication = (Component) => {
-  const WithAuthentication = (props) => {
-    const [authUser, setAuthUser] = useState(null);
+	const WithAuthentication = (props) => {
+		const [authUser, setAuthUser] = useState(null);
 
-    useEffect(() => {
-      //get the authenticated user from Firebase
-      let listener = props.firebase.auth.onAuthStateChanged((authUser) => {
-        if (authUser) {
-          const getUserData = async () => {
-            const userData = await props.firebase.getUserByID(authUser.uid);
-            authUser.data = userData;
-            setAuthUser({ authUser });
-          };
-          getUserData();
-        } else {
-          setAuthUser(null);
-        }
-      });
+		useEffect(() => {
+			//get the authenticated user from Firebase
+			let listener = props.firebase.auth.onAuthStateChanged(
+				(authUser) => {
+					if (authUser) {
+						const getUserData = async () => {
+							const userData =
+								await props.firebase.getUserByID(
+									authUser.uid
+								);
 
-      return () => {
-        listener();
-      };
-    }, [props.firebase.auth, props.firebase]);
+							const userClaims =
+								await authUser.getIdTokenResult();
 
-    return (
-      <AuthUserContext.Provider value={authUser}>
-        <Component {...props} />
-      </AuthUserContext.Provider>
-    );
-  };
-  return withFirebase(WithAuthentication);
+							authUser.admin = userClaims.claims.admin;
+							authUser.data = userData;
+							setAuthUser({ authUser });
+						};
+						getUserData();
+					} else {
+						setAuthUser(null);
+					}
+				}
+			);
+
+			return () => {
+				listener();
+			};
+		}, [props.firebase.auth, props.firebase]);
+
+		return (
+			<AuthUserContext.Provider value={authUser}>
+				<Component {...props} />
+			</AuthUserContext.Provider>
+		);
+	};
+	return withFirebase(WithAuthentication);
 };
 
 export default withAuthentication;
