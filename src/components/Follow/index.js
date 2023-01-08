0323689d99e0dd9e52e2396c73c5aefbbb335332
johanpaolorodriguez <johanpaolorodriguez@gmail.com>
2023-01-09 00:42:34 +0800

    chore(FollowButton): change design and functions

--- src/components/Follow/index.js
+++ src/components/Follow/index.js
@@ -1,49 +1,80 @@
-import { useContext } from "react";
-import { useHistory } from "react-router-dom";
+import { useEffect, useContext, useState } from "react";
 import { BookmarkIcon } from "@heroicons/react/outline";
 import { BookmarkIcon as BookmarkIconFilled } from "@heroicons/react/solid";
 import { withFirebase } from "../Firebase";
 import { AuthUserContext } from "../Session";
+import { onSnapshot, doc } from "@firebase/firestore";
 
-const FollowButton = ({ firebase, setIsOpen, startupUid, userData }) => {
-  const authUser = useContext(AuthUserContext);
+const FollowButton = ({ firebase, setIsOpen, startupUid }) => {
+	const authUser = useContext(AuthUserContext);
+	const [snapshotData, setSnapshotData] = useState(null);
 
-  const userIsFollowingStartup = () => {
-    if (userData && userData.following) {
-      return userData.following.includes(startupUid);
-    } else {
-      return false;
-    }
-  };
+	const userIsFollowingStartup = () => {
+		if (snapshotData) {
+			return snapshotData.following.includes(startupUid);
+		} else {
+			return false;
+		}
+	};
+	useEffect(() => {
+		const getUserData = () => {
+			if (authUser) {
+				try {
+					const unsubscribe = onSnapshot(
+						doc(firebase.db, "users", authUser.authUser.uid),
+						(doc) => {
+							setSnapshotData(doc.data());
+						}
+					);
+				} catch (error) {
+					console.log(error);
+				}
+			}
+		};
+		getUserData();
+	}, [authUser, firebase.db]);
 
-  const handleOnClick = async () => {
-    if (authUser === null) {
-      setIsOpen(true);
-      return;
-    }
-    try {
-      if (userIsFollowingStartup()) {
-        await firebase.doUnfollowStartupAsUser(
-          authUser.authUser.uid,
-          startupUid
-        );
-      } else {
-        await firebase.doFollowStartupAsUser(authUser.authUser.uid, startupUid);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const handleOnClick = async () => {
+		if (authUser === null) {
+			setIsOpen(true);
+			return;
+		}
 
-  return (
-    <button onClick={() => handleOnClick()}>
-      {userIsFollowingStartup() ? (
-        <BookmarkIconFilled className="w-6 h-6 text-blue-500" />
-      ) : (
-        <BookmarkIcon className="w-6 h-6 text-blue-500" />
-      )}
-    </button>
-  );
+		try {
+			if (userIsFollowingStartup()) {
+				await firebase.doUnfollowStartupAsUser(
+					authUser.authUser.uid,
+					startupUid
+				);
+			} else {
+				await firebase.doFollowStartupAsUser(
+					authUser.authUser.uid,
+					startupUid
+				);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<button
+			onClick={() => handleOnClick()}
+			className="flex justify-center w-full px-8 py-4 text-blue-500 bg-white border border-blue-500 rounded-md text-button font-primary space-x-2 items-center | md:order-first"
+		>
+			{userIsFollowingStartup() ? (
+				<>
+					<BookmarkIconFilled className="w-6 h-6 text-blue-500" />
+					<span className="">Keep me updated</span>
+				</>
+			) : (
+				<>
+					<BookmarkIcon className="w-6 h-6 text-blue-500" />
+					<span className="">Keep me updated</span>
+				</>
+			)}
+		</button>
+	);
 };
 
 export default withFirebase(FollowButton);
