84314120f2c77f6e593d649fd235cac388b7bf84
johanpaolorodriguez <johanpaolorodriguez@gmail.com>
2023-01-09 00:40:33 +0800

    chore(routes): add sign up routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,10 @@
 export const LANDING = "/";
 export const STARTUPS = "/directory";
 export const SIGN_UP = "/signup";
+export const SIGN_UP_ABOUT = "/signup/about";
+export const SIGN_UP_INTERESTS = "/signup/interests";
+export const SIGN_UP_TALENT = "/signup/talent";
+export const SIGN_UP_CONTRIBUTIONS = "/signup/contributions";
 export const SIGN_IN = "/signin";
 export const HOME = "/home";
 export const ACCOUNT = "/account";
@@ -11,10 +15,10 @@ export const CATEGORY = "/categories/:paramFilter";
 export const PROFILE = "/user/:uid";
 
 export const ITEMS = [
-  { name: "Directory", href: STARTUPS, current: false, restricted: true },
-  // { name: "Home", href: HOME, current: false, restricted: true },
-  // { name: "Account", href: ACCOUNT, current: false, restricted: true },
-  // { name: "Admin", href: ADMIN, current: false, restricted: true },
-  { name: "Sign In", href: SIGN_IN, current: false, restricted: false },
-  { name: "Sign Up", href: SIGN_UP, current: false, restricted: false },
+	{ name: "Directory", href: STARTUPS, current: false, restricted: true },
+	// { name: "Home", href: HOME, current: false, restricted: true },
+	// { name: "Account", href: ACCOUNT, current: false, restricted: true },
+	// { name: "Admin", href: ADMIN, current: false, restricted: true },
+	{ name: "Sign In", href: SIGN_IN, current: false, restricted: false },
+	{ name: "Sign Up", href: SIGN_UP, current: false, restricted: false },
 ];
