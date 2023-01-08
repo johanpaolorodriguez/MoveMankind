f330bbb09aeb5e3f26399606a5e1c31952a9cc8f
johanpaolorodriguez <johanpaolorodriguez@gmail.com>
2023-01-09 00:46:43 +0800

    chore(SignOut): change to forwardref

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { withFirebase } from "../Firebase";
 
-const SignOutButton = ({ firebase }) => (
-  <button
-    type="button"
-    className="block w-full px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-100"
-    onClick={firebase.doSignOut}
-  >
-    Sign Out
-  </button>
-);
+const SignOutButton = React.forwardRef((props, ref) => {
+	let { firebase, rest } = props;
+
+	return (
+		<button
+			type="button"
+			ref={ref}
+			{...rest}
+			className="block w-full px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-100"
+			onClick={firebase.doSignOut}
+		>
+			Sign Out
+		</button>
+	);
+});
 
 export default withFirebase(SignOutButton);
