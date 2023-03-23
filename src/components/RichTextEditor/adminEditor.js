31be39ab9220eac2b49434c0efe6348934f3023c
johanpaolorodriguez <johanpaolorodriguez@gmail.com>
2023-03-23 19:47:54 +0800

    fix(toolbar): remove transform property from parent to enable fixed positioning of child

--- src/components/RichTextEditor/adminEditor.js
+++ src/components/RichTextEditor/adminEditor.js
@@ -31,10 +31,10 @@ export default function AdminEditor({
 			>
 				<Transition.Child
 					as={Fragment}
-					enter="ease-out duration-300"
+					enter="ease-out duration-500"
 					enterFrom="opacity-0"
 					enterTo="opacity-100"
-					leave="ease-in duration-200"
+					leave="ease-in duration-500"
 					leaveFrom="opacity-100"
 					leaveTo="opacity-0"
 				>
@@ -45,14 +45,14 @@ export default function AdminEditor({
 					<div className="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
 						<Transition.Child
 							as={Fragment}
-							enter="ease-out duration-300"
-							enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-							enterTo="opacity-100 translate-y-0 sm:scale-100"
-							leave="ease-in duration-200"
-							leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-							leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+							enter="ease-out duration-500"
+							enterFrom="opacity-0"
+							enterTo="opacity-100"
+							leave="ease-in duration-500"
+							leaveFrom="opacity-100"
+							leaveTo="opacity-0"
 						>
-							<Dialog.Panel className="relative min-h-[75vh] overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:w-full sm:max-w-screen-xl">
+							<Dialog.Panel className="relative min-h-[75vh] overflow-hidden text-left bg-white rounded-lg shadow-xl sm:my-8 sm:w-full sm:max-w-screen-xl">
 								<div className="px-4 py-3 sm:flex sm:px-6">
 									<input
 										value={title}
