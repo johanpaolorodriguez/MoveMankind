eb2eb317eb3773ecbf2dd590e7c296e507f0ea0d
johanpaolorodriguez <johanpaolorodriguez@gmail.com>
2023-04-08 15:21:58 +0800

    refactor(Banner): to accept and display props dynamically

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,22 +1,20 @@
-export default function Banner({ setAdminEditorOpen, setOrderSectionsOpen }) {
+export default function Banner(props) {
+	const { functions, title } = props;
 	return (
 		<div className="bg-[#1b2733] rounded-md">
 			<div className="px-3 py-3 mx-auto max-w-7xl sm:px-6 lg:px-8">
 				<div className="flex flex-wrap items-center justify-center space-x-6 text-center text-white">
-					<span>Add or Edit the sections of this page</span>
-					<button
-						onClick={() => setAdminEditorOpen(true)}
-						className="flex justify-center px-4 py-2 text-base text-blue-500 bg-white border border-blue-500 rounded-md text-button font-primary"
-					>
-						Open Editor
-					</button>
+					{title && <span>{title}</span>}
 
-					<button
-						onClick={() => setOrderSectionsOpen(true)}
-						className="flex justify-center px-4 py-2 text-base text-blue-500 bg-white border border-blue-500 rounded-md text-button font-primary"
-					>
-						Order Sections
-					</button>
+					{functions?.map((fn) => (
+						<button
+							key={fn.function}
+							onClick={() => fn.function(true)}
+							className="flex justify-center px-4 py-2 text-base text-blue-500 bg-white border border-blue-500 rounded-md text-button font-primary"
+						>
+							{fn.content}
+						</button>
+					))}
 				</div>
 			</div>
 		</div>
