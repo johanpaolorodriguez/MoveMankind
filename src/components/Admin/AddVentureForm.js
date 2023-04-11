ae233ebaf522984971e63a794788921404ec9e54
johanpaolorodriguez <johanpaolorodriguez@gmail.com>
2023-04-11 23:39:27 +0800

    fix(AddVentureForm): remove toast

--- src/components/Admin/AddVentureForm.js
+++ src/components/Admin/AddVentureForm.js
@@ -7,37 +7,6 @@ import { Input } from "../SignUp/forms/Input";
 import { Button } from "../SignUp/forms/Button";
 import FileInput from "../SignUp/forms/FileInput";
 
-const customToast = (text) => {
-	return toast.custom((t) => (
-		<div
-			className={`${
-				t.visible ? "animate-enter" : "animate-leave"
-			} max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-		>
-			<div className="flex-1 w-0 p-4">
-				<div className="flex items-start">
-					<div className="flex-shrink-0 pt-0.5">
-						<CheckCircleIcon className="w-10 h-10 text-green-500 rounded-full" />
-					</div>
-					<div className="flex-1 ml-3">
-						<p className="text-sm font-medium text-gray-900">
-							{text}
-						</p>
-					</div>
-				</div>
-			</div>
-			<div className="flex border-l border-gray-200">
-				<button
-					onClick={() => toast.dismiss(t.id)}
-					className="flex items-center justify-center w-full p-4 text-sm font-medium text-indigo-600 border border-transparent rounded-none rounded-r-lg hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-				>
-					Close
-				</button>
-			</div>
-		</div>
-	));
-};
-
 export default function AddVentureForm({ isOpen, setIsOpen, saveStartupToDB }) {
 	const cancelButtonRef = useRef(null);
 	const {
